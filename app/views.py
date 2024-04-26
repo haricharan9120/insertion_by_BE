09b51a83863afb606caa81f4e2bc2e8d87cbcbcd
haricharan9120 <haricharan9120@gmail.com>
2024-04-26 16:57:17 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -6,13 +6,12 @@ from app.models import *
 
 def insert_topic(request):
     tn=input()
-    TO=Topic.objects.get_or_create(topic_name=tn)[0]
-    TO.save()
+    TO=Topic.objects.get_or_create(topic_name=tn)
     return HttpResponse('TOPIC IS CREATED')
 
 
 def insert_Webpage(request):
-    tn=input()
+    '''tn=input()
     TO=Topic.objects.get_or_create(topic_name=tn)[0]
     TO.save()
     n=input()
@@ -21,6 +20,24 @@ def insert_Webpage(request):
     WO=Webpage.objects.get_or_create(topic_name=TO,name=n,url=u,email=e)[0]
     WO.save()
     return HttpResponse('WEBPAGE CREATED')
+    
+    tn=input()
+    LTO=Topic.objects.get(topic_name=tn)
+    '''
+
+    tn=input()
+    n=input()
+    u=input()
+    e=input()
+    LTO=Topic.objects.filter(topic_name=tn)
+    if LTO:
+        TO=LTO[0]
+        WO=Webpage.objects.get_or_create(topic_name=TO,name=n,url=u,email=e)[0]
+        WO.save()
+        return HttpResponse('WEBPAGE IS CREATED')
+    else:
+        return HttpResponse('NO SUCH TOPIC IN PARENT TABLE')
+    
 
 
 def insert_Access(request):
